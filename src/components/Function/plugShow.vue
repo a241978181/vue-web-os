b223757117d0439bfcaee4199922fec2b6a92e56
李建 <a2417978181@163.com>
2021-01-15 14:51:48 +0800

    新增组件展示页面

---
+++ src/components/Function/plugShow.vue
@@ -0,0 +1,129 @@
+<template>
+    <div>
+        <Modal width="900" id="modalView" :fullscreen="fullscreen" v-model="isViewBool" footer-hide
+               :draggable="draggable" :closable="false">
+            <!-- 头-->
+            <FuctionHeader slot="header" @mini="mini" @big="big" @small="small" @closeView="closeView"
+                           :fullscreen="fullscreen" :menu="menu"></FuctionHeader>
+
+            <!-- 主体 -->
+            <div style="width: 100%;display: flex;flex-direction: column;align-items: center;">
+                <div style="width: 100%;display: flex;align-items: center;">
+                    <el-tooltip class="item" effect="dark" content="刷新" placement="bottom">
+                        <a> <Icon @click="refresh" style="margin-right: 5px" size="25" type="md-aperture" /></a>
+                    </el-tooltip>
+                    <!--标题栏-->
+                    <FuctionTitle @openMenu="openMenu" :permissionsList="permissionsList"
+                                  :indexButton="indexButton"></FuctionTitle>
+                </div>
+                <component @routerTo="openMenu2(arguments)" :is="allComps[permissionsItem.permissionsenglish]"
+                           :menu="permissionsItem"></component>
+            </div>
+        </Modal>
+    </div>
+</template>
+
+<script>
+    //有了页面之后记得修改一下这里的导入路径哦！！！
+    import allComps from '../Menu/plugShow/index.js'
+    import {getPermissionsList} from '@/utils/permissions.js'
+    import FuctionHeader from "@/components/util/FuctionHeader";
+    import FuctionTitle from "@/components/util/FuctionTitle";
+
+    export default {
+        name: "plugShow",
+        props: {
+            menu: '',
+        },
+        components: {
+            FuctionHeader,
+            FuctionTitle
+        },
+        data() {
+            return {
+                //该属性为权限属性，需要修改为自己的权限英文名称，一般和当前页面name对应
+                ItemName: 'plugShow',
+                // 组件集合
+                allComps: allComps,
+                //当前组件
+                permissionsItem: '',
+                //全屏
+                fullscreen: true,
+                //移动
+                draggable: false,
+                //当前菜单按钮
+                indexButton: null,
+                //菜单
+                permissionsList: getPermissionsList(2, this.menu.id),
+            }
+        },
+        computed: {
+            //判断是否展示该面板
+            isViewBool: {
+                get() {
+                    return this.$store.state.control[this.ItemName];
+                },
+                set(v) {
+                    this.$store.commit("setFalseVB", this.ItemName);
+                }
+            },
+        },
+        methods: {
+            //刷新
+            refresh(){
+                let oldIndex=this.indexButton;
+                let oldPermissionsItem=this.permissionsItem;
+                this.openMenu({},-1)
+                this.timer = setTimeout(()=>{   //设置延迟执行,刷新必须，0毫秒为无延迟
+                    this.openMenu(oldPermissionsItem,oldIndex)
+                },0);
+            },
+            //子组件调用打开菜单
+            openMenu2(values) {
+                this.indexButton = values[1];
+                this.permissionsItem = values[0];
+            },
+            //打开菜单
+            openMenu(item, index) {
+                this.indexButton = index;
+                this.permissionsItem = item;
+            },
+            //关闭对话框
+            closeView() {
+                this.$store.commit("setFalseVB", this.ItemName);
+                this.$store.commit("deleteTaskList", this.menu);
+            },
+            //缩小对话框
+            small() {
+                this.fullscreen = false
+                this.draggable = true
+            },
+            //放大对话框
+            big() {
+                this.fullscreen = true
+                this.draggable = false
+            },
+            //最小化
+            mini() {
+                this.$store.commit("setFalseVB", this.ItemName);
+            },
+        },
+        mounted() {
+        }
+    }
+</script>
+
+<style scoped="scoped">
+    .BTL {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
+<style>
+    #modalView .ivu-modal-header {
+        background-color: #348DC1;
+        padding: 5px;
+    }
+</style>
---
+++ src/components/Menu/plugShow/slidingList.vue
@@ -0,0 +1,141 @@
+<template>
+    <div id="view">
+        <div style="width: 100%;display: flex;align-items: center;justify-content: center"><h2>vue-draggable组件展示</h2></div>
+        <div style="width: 100%;display: flex;align-items: flex-start;justify-content: space-between">
+            <!--使用draggable组件-->
+                <div class="col">
+                    <div class="title" >蜀国</div>
+                    <draggable v-model="arr1" group="site"  animation="300" dragClass="dragClass"  ghostClass="ghostClass" chosenClass="chosenClass" @start="onStart" @end="onEnd">
+                        <transition-group>
+                            <div class="item" v-for="item in arr1" :key="item.id">{{item.name}}</div>
+                        </transition-group>
+                    </draggable>
+                </div>
+                <div  class="col">
+                    <div class="title" >吴国</div>
+                    <draggable v-model="arr2" group="site" animation="300" dragClass="dragClass"  ghostClass="ghostClass" chosenClass="chosenClass" @start="onStart" @end="onEnd">
+                        <transition-group>
+                            <div class="item" v-for="item in arr2" :key="item.id">{{item.name}}</div>
+                        </transition-group>
+                    </draggable>
+                </div>
+                <div  class="col">
+                    <div class="title" >魏国</div>
+                    <draggable v-model="arr3" group="site" animation="300" dragClass="dragClass"  ghostClass="ghostClass" chosenClass="chosenClass" @start="onStart" @end="onEnd">
+                        <transition-group>
+                            <div class="item" v-for="item in arr3" :key="item.id">{{item.name}}</div>
+                        </transition-group>
+                    </draggable>
+                </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import draggable from "vuedraggable"
+    export default {
+        name: "slidingList",
+        components: {
+            draggable
+        },
+        props: {
+            menu: '',
+        },
+        data() {
+            return {
+                drag:false,
+                //定义要被拖拽对象的数组
+                arr1:[
+                    {id:1,name:'张飞'},
+                    {id:2,name:'诸葛亮'},
+                    {id:3,name:'刘备'},
+                    {id:4,name:'关羽'}
+                ],
+                arr2:[
+                    {id:5,name:'曹操'},
+                    {id:6,name:'曹丕'},
+                    {id:7,name:'张辽'},
+                    {id:8,name:'郭嘉'}
+                ],
+                arr3:[
+                    {id:9,name:'孙权'},
+                    {id:10,name:'周瑜'},
+                    {id:11,name:'鲁肃'},
+                    {id:12,name:'陆逊'}
+                ]
+            }
+        },
+        methods: {
+            //开始拖拽事件
+            onStart(){
+                this.drag=true;
+            },
+            //拖拽结束事件
+            onEnd() {
+                this.drag=false;
+            },
+        },
+        mounted() {
+        }
+    }
+</script>
+
+<style scoped>
+    #view {
+        width: 100%;
+        height: 100%;
+        padding: 0px;
+        margin: 0px;
+        margin-top: 0.9375rem;
+    }
+
+    /*定义要拖拽元素的样式*/
+    .ghostClass{
+        background-color:  blue !important;
+    }
+    .chosenClass{
+        background-color: red !important;
+        opacity: 1!important;
+    }
+    .dragClass{
+        background-color: blueviolet !important;
+        opacity: 1 !important;
+        box-shadow:none !important;
+        outline:none !important;
+        background-image:none !important;
+    }
+    .itxst{
+        margin: 10px;
+
+    }
+    .title{
+        padding: 6px 12px;
+    }
+    .col{
+        width: 40%;
+        flex: 1;
+        padding: 10px;
+        border: solid 1px #eee;
+        border-radius:5px ;
+        float: left;
+    }
+    .col+.col{
+        margin-left: 10px;
+    }
+
+    .item{
+        padding: 6px 12px;
+        margin: 0px 10px 0px 10px;
+        border:  solid 1px #eee;
+        background-color: #f1f1f1;
+    }
+    .item:hover{
+        background-color: #fdfdfd;
+        cursor: move;
+    }
+    .item+.item{
+        border-top:none ;
+        margin-top: 6px;
+    }
+</style>
